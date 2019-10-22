9fec8713a2b82051ee822f1ee3684ab7e520f358
jarek ratajski <jratajski@gmail.com>
2019-10-22 15:15:12 -0700

    better test of combination of effects

---
+++ nee-core/src/test/kotlin/pl/setblack/nee/effects/tx/DeeperEffectTest.kt
@@ -0,0 +1,49 @@
+package pl.setblack.nee.effects.tx
+
+import io.kotlintest.be
+import io.kotlintest.should
+import io.kotlintest.specs.BehaviorSpec
+import pl.setblack.nee.NEE
+import pl.setblack.nee.andThen
+
+class DeeperEffectTest  : BehaviorSpec ({
+    Given("TestEffect") {
+        val log = mutableListOf<String>()
+        val eff = TestEffect("ef1", log)
+        val nee = NEE.Companion.pure(eff, function1(log))
+        When("called") {
+            val res = nee.perform(TestResource(1))(Unit)
+            Then ("log is ok") {
+                ///TODO() write expectations on order of items in log
+                println(res)
+                println(log)
+                res.get() should be ("OK")
+            }
+        }
+    }
+    Given("Two TestEffects") {
+        val log = mutableListOf<String>()
+        val eff1 = TestEffect("ef1", log)
+        val eff2 = TestEffect("ef2", log)
+        val eff = eff1.andThen(eff2)
+        val nee = NEE.Companion.pure(eff, function1(log))
+        When("called") {
+            val res = nee.perform(TestResource(1))(Unit)
+            Then ("log is ok") {
+                println(res)
+                println(log)
+                res.get() should be ("OK")
+            }
+        }
+    }
+
+}) {
+    companion object {
+        fun function1(log : MutableList<String>) = { db: TestResource ->
+            { _:Unit ->
+                log.add("function1 called with: $db")
+                 "OK"
+            }
+        }
+    }
+}
