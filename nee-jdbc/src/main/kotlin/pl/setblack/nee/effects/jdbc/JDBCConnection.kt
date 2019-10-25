c0133f654f5cbe3a1c297dd2e59e77b63b920aca
jarek ratajski <jratajski@gmail.com>
2019-10-25 15:24:41 -0700

    jdbc work continues

--- nee-jdbc/src/main/kotlin/pl/setblack/nee/effects/jdbc/JDBCConnection.kt
+++ nee-jdbc/src/main/kotlin/pl/setblack/nee/effects/jdbc/JDBCConnection.kt
@@ -1,6 +1,7 @@
 package pl.setblack.nee.effects.jdbc
 
 import io.vavr.control.Either
+import io.vavr.control.Option
 import pl.setblack.nee.Logging
 import pl.setblack.nee.effects.tx.TxConnection
 import pl.setblack.nee.effects.tx.TxError
@@ -8,7 +9,6 @@ import pl.setblack.nee.effects.tx.TxStarted
 import pl.setblack.nee.logger
 import java.sql.Connection
 import java.sql.DriverManager
-import java.sql.SQLException
 import java.util.concurrent.atomic.AtomicReference
 
 class JDBCConnection(private val cfg: JDBCConfig) : TxConnection<Connection>, Logging {
@@ -23,13 +23,15 @@ class JDBCConnection(private val cfg: JDBCConfig) : TxConnection<Connection>, Lo
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun cont(): Either<TxError, TxStarted<Connection>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun cont(): Either<TxError, TxStarted<Connection>> =
+        Either.right<TxError, TxStarted<Connection>>(JDBCTransaction(this)).also {
+            if (!hasTransaction()) {
+                getResource().autoCommit = false
+            }
+        }
 
-    override fun hasTransaction(): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun hasTransaction(): Boolean = !this.getResource().autoCommit
+
 
     override fun getResource(): Connection =
         this.connectionRef.get().let { connection ->
@@ -37,7 +39,25 @@ class JDBCConnection(private val cfg: JDBCConfig) : TxConnection<Connection>, Lo
             connection
         }
 
-    override fun close() = getResource().close()
+    override fun close(): Unit = getResource().let { conn ->
+        if (conn.isClosed) {
+            logger().warn("connection already closed")
+        } else {
+            conn.close()
+        }
+    }
+}
+
+class JDBCTransaction(val conn: JDBCConnection) : TxConnection<Connection> by conn,
+    TxStarted<Connection> {
+    override fun commit(): Pair<Option<TxError>, TxConnection<Connection>> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun rollback(): Pair<Option<TxError>, TxConnection<Connection>> =
+        getResource().rollback().let {
+            Pair(Option.none(), conn)
+        }
 
 }
 
--- nee-jdbc/src/test/kotlin/pl/setblack/nee/effects/jdbc/JDBCConnectionTest.kt
+++ nee-jdbc/src/test/kotlin/pl/setblack/nee/effects/jdbc/JDBCConnectionTest.kt
@@ -1,12 +1,15 @@
 package pl.setblack.nee.effects.jdbc
 
-import io.kotlintest.specs.DescribeSpec
 import io.kotlintest.shouldBe
+import io.kotlintest.specs.DescribeSpec
 import io.kotlintest.shouldNotBe
+import java.sql.Connection
+import java.sql.ResultSet
+import io.vavr.collection.List
 
-class JDBCConnectionTest : DescribeSpec( {
+class JDBCConnectionTest : DescribeSpec({
     describe("jdbc connection") {
-        val cfg = JDBCConfig("org.h2.Driver", "jdbc:h2:mem:test_mem","sa")
+        val cfg = JDBCConfig("org.h2.Driver", "jdbc:h2:mem:test_mem", "sa")
         context("connection") {
             val conn = JDBCConnection(cfg)
             it("is created") {
@@ -15,6 +18,99 @@ class JDBCConnectionTest : DescribeSpec( {
             it("gives access to physical jdbc") {
                 conn.getResource() shouldNotBe null
             }
+            it("reads from dual") {
+                val res = simpleQuery(conn.getResource(), "select 4 from dual")
+                res[0] shouldBe "4"
+            }
+            it("should start with no transaction") {
+                conn.hasTransaction() shouldBe false
+            }
+            it("connection should close ") {
+                conn.close()
+            }
+        }
+        context("creation of data") {
+            val conn = JDBCConnection(cfg)
+            it("should create table") {
+                val res = simpleUpdate(
+                    conn.getResource(),
+                    """ CREATE TABLE PLANETS (
+                            	ID INT not null,
+	                            NAME VARCHAR not null,
+                                PRIMARY KEY (ID));
+                    """
+                )
+                res shouldBe 0
+            }
+            it("should insert row") {
+                val res = simpleUpdate(
+                    conn.getResource(),
+                    """ INSERT INTO PLANETS VALUES (1,'MERCURY')"""
+                )
+                res shouldBe 1
+            }
+            it("should read single row") {
+                val res = conn.getResource().q(
+                    "SELECT NAME FROM PLANETS"
+                )
+                res[0] shouldBe "MERCURY"
+            }
+
+        }
+        context("transactions") {
+            val conn = JDBCConnection(cfg)
+            context("simple trx") {
+                val trx = conn.cont()
+                it("should start  trx") {
+                    trx.isRight shouldBe (true)
+                }
+                context("a") {
+                    val trConnection = trx.get()
+                    it("should insert row") {
+                        val res = trConnection.getResource().x(" INSERT INTO PLANETS VALUES (2,'VENUS')")
+                        res shouldBe 1
+                    }
+                    it ("should read this row") {
+                        val res = trConnection.getResource().q(
+                            "SELECT NAME FROM PLANETS WHERE ID = 2;")
+                        res[0] shouldBe ("VENUS")
+                    }
+                    it("after rollback") {
+                        trConnection.rollback()
+                        val res = conn.getResource().q(
+                            "SELECT COUNT(ID) FROM PLANETS")
+                        res[0] shouldBe ("1")
+                    }
+                }
+
+
+            }
         }
     }
 })
+
+
+fun simpleQuery(jdbcConnection: Connection, sql: String) = jdbcConnection.let { conn ->
+    conn.createStatement().use { stmt ->
+        val result = stmt.executeQuery(sql)
+        resultToRow(result, List.empty())
+    }
+}
+
+
+fun simpleUpdate(jdbcConnection: Connection, sql: String) = jdbcConnection.let { conn ->
+    conn.createStatement().use { stmt ->
+        stmt.executeUpdate(sql)
+    }
+}
+
+fun Connection.q(sql: String) = simpleQuery(this, sql)
+fun Connection.x(sql: String) = simpleUpdate(this, sql)
+
+tailrec fun resultToRow(resultSet: ResultSet, rows: List<String>): List<String> =
+    if (!resultSet.next()) {
+        rows
+    } else {
+        val stringRes = resultSet.getString(1)
+        resultToRow(resultSet, rows.append(stringRes))
+    }
