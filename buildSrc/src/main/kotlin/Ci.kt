56ef9efc1fabd860c87aadb73d05a8116ee4a71f
jarek ratajski <jratajski@gmail.com>
2020-10-13 01:31:48 +0200

    release 0.4.5 minor fixes

--- buildSrc/src/main/kotlin/Ci.kt
+++ buildSrc/src/main/kotlin/Ci.kt
@@ -2,7 +2,7 @@ object Ci {
 
     // this is the version used for building snapshots
     // .buildnumber-snapshot will be appended
-    private const val snapshotBase = "0.4.5"
+    private const val snapshotBase = "0.4.6"
 
     private val githubBuildNumber = System.getenv("GITHUB_RUN_NUMBER")
 
